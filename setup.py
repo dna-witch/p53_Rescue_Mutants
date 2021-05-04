a373600b50229d417c6bb50fefac3a2755bfe9d8
dna-witch <shakuntala.mitra@gmail.com>
2021-05-03 23:56:19 -0700

    add files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='this project aims to identify mutants that could lead to functional rescue and regions of the p53 core domain that could be altered to rescue function by modeling mutant p53 transcriptional activity',
+    author='Shakuntala Mitra',
+    license='MIT',
+)
